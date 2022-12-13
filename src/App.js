eb0c96cee7f931546ddafe7411df7d2713edebda
josephpogue <63912158+josephpogue@users.noreply.github.com>
2022-12-13 13:20:01 -0500

    Footer placeholder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <NavBar />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className="footer">
+            <div className="footer-icons">
+                footer icons
+            </div>
+            <div>
+                <p className="copyright-text">
+                    Copyright 2022 My Website
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
